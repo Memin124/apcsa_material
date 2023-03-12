cf36c05a546d713c91db66e622174ff7bf1e4a39
Mehmet Colak <107381866+Memin124@users.noreply.github.com>
2023-03-12 18:44:16 -0400

    Update Driver.java

--- classwork/2023-03-08/Driver.java
+++ classwork/2023-03-08/Driver.java
@@ -1,10 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-public class Driver{
-  public static void main(String[] args) {
-    MyLinkedList myList = new MyLinkedList();
-    
-    System.out.println(myList);
-  }
+public class Driver {
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+
+        // Add some elements to the list
+        list.add("apple");
+        list.add("banana");
+        list.add("cherry");
+        list.add("date");
+
+        // Test the add method
+        System.out.println("List after adding elements: " + list);
+
+        // Test the get method
+        System.out.println("Element at index 2: " + list.get(2));
+
+        // Test the size method
+        System.out.println("List size: " + list.size());
+
+        // Test the add method with index
+        list.add(1, "pear");
+        System.out.println("List after adding element at index 1: " + list);
+
+        // Test the indexOf method
+        System.out.println("Index of \"cherry\": " + list.indexOf("cherry"));
+
+        // Test the remove method
+        list.remove(2);
+        System.out.println("List after removing element at index 2: " + list);
+        
+        list.remove(0);
+        System.out.println("List after removing element at index 0: " + list);
+        
+        list.remove(2);
+        System.out.println("List after removing element at index 2: " + list);
+    }
 }
