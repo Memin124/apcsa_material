0c2670d1b7cb15b0c42a6387694676ab5c094cd0
Mehmet Colak <107381866+Memin124@users.noreply.github.com>
2023-03-24 08:41:39 -0400

    Create Stack.java

---
+++ classwork/2023-03-24/Stack.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Stack{
+  // This Stack class is designed for integer values >= 0
+  private Node<Integer> top;
+  // Instance variables here
+  private int size;
+  
+  // Constructor
+  public Stack(){
+    top = null;
+    size = 0;
+  }   
+
+  // Add element to stack
+  public void push(int value){
+    //Node newTop = CONTINUE FROM HERE
+  }
+
+  // Remove element from stack  
+  public int pop(){
+
+  }
+
+  // Get top element
+  public int top(){
+
+  }
+
+  // Check is stack empty
+  public boolean isEmpty(){
+
+  }
+
+  // Return size stack
+  public int size(){
+  
+  }
+
+  // Print stack
+  public String toString(){
+
+  }
+
+
+}
